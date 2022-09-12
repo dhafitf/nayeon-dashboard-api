5f6eebd52c121b8b23b53801832e0f75220d196d
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-09-12 16:04:14 +0700

    update filterByPermission

--- src/services/guilds/getMutualGuildsService.ts
+++ src/services/guilds/getMutualGuildsService.ts
@@ -4,7 +4,13 @@ import { PartialGuilds } from "../../types/guilds";
 export default async function getMutualGuildsService(userGuilds: PartialGuilds[]) {
   const { data: botGuilds } = await getBotGuildsService();
 
-  const adminUserGuilds = userGuilds.filter(({ permissions }) => (parseInt(permissions) & 0x20) === 0x20);
+  const filterByPermission = (item: PartialGuilds) => {
+    if ((parseInt(item.permissions) & 0x20) === 0x20) return (item.manageGuild = true);
+    return (item.manageGuild = false);
+  };
+
+  const adminUserGuilds = userGuilds.filter(filterByPermission);
+
   const mutualGuilds = adminUserGuilds.map((guild) => {
     if (Boolean(botGuilds.some((botGuild) => botGuild.id === guild.id))) {
       return { ...guild, mutual: true };
